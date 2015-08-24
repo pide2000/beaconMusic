4df1c0e348e20f7f489a6643cced02bc93522862
Peter Bolch <bolch@punkt.de>
2015-08-24 22:32:22 +0200

    Some small changes on main program

--- index.js
+++ index.js
@@ -4,26 +4,42 @@ var open = require('open');
 
 
 noble.on('stateChange', function(state) {
+
   if (state === 'poweredOn') {
     noble.startScanning(['feaa'],false);
+
+    /**
+    * Use this Command to allow duplicates.
+    * This means, the beacon will be detected everytime it broadcasts the data
+    * noble.startScanning(['feaa'],true);
+    **/
+
   } else {
     noble.stopScanning();
   }
+
 });
 
 noble.on('scanStart', function() {
+
   console.log('Scan started...');
-  console.log();
+
 });
 
 noble.on('scanStop', function() {
+
   console.log('Scan stopped...');
   console.log();
+
 });
 
 noble.on('discover', function(peripheral) {
+
   var serviceData = peripheral.advertisement.serviceData;
+
+  console.log("Peripherial");
   console.log(peripheral);
+
   if (serviceData && serviceData.length) {
     var objects = [];
     for (var i in serviceData) {
@@ -31,7 +47,7 @@ noble.on('discover', function(peripheral) {
       if (serviceData[i].data.toString('hex').substr(0,2) === '10') {
         var url = urldecode(serviceData[i].data.toString('hex'));
         console.log(serviceData[i].data.toString('hex'));
-
+        console.log(url);
       }
     }
   }
