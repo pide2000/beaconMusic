f263cef234dd2cb83bc6d829e4c1ec3d7d534adb
Peter Bolch <bolch@punkt.de>
2015-12-30 09:44:47 +0100

    [FIX] ressendfile needs an absolute path

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ io.on('connection', function (socket) {
 app.use("/", express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-	res.sendFile('index.html');
+	res.sendFile(__dirname + '/public/index.html');
 });
 
 //return a list of all discovered beacons
